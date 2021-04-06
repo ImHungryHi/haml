eab8589e194a77faf622ef3db5938e6cdcd52949
jeroennoyez <jeroennoyez@gmail.com>
2021-04-07 00:07:11 +0200

    Added main calls in App

--- src/main/java/be/syntra/beige/team/App.java
+++ src/main/java/be/syntra/beige/team/App.java
@@ -1,13 +1,67 @@
 package be.syntra.beige.team;
 
+import java.io.*;
+
 /**
- * Hello world!
+ * <h1>App</h1>
+ * Read command from console.
+ * Create ReaderHaml
+ * Create HamlData
+ * Validate HamlData
+ * Create Html
+ * Write Html to file
  *
+ *
+ * @author  Team Beige
+ * @version 1.0
+ * @since   2021-03-24
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    public static void main(String[] args) throws IOException {
+
+        // Read command from console
+        //
+        // -- code here --
+
+
+        // Construct ReaderHaml with parameters from console
+        // Params: .haml input filename ; .html output filename (optional)
+        // Temporary:
+        // the example.haml input file & example.html output file are hardcoded in ReaderHaml
+        //
+        ReaderHaml rh = new ReaderHaml();
+        HamlData hd = rh.read();
+
+        // Temporary:
+        // Print hamlData to console
+        System.out.println(hd);
+
+
+        // Validate HamlData object
+        //
+        if(HamlValidation.validateHaml(hd)){
+
+            // Create Html object
+            Html html = new Html(hd.getInputFileName(), hd.getOutputFileName());
+            // Pass HamlDataElements to HtmlConverter
+            HtmlConverter.convertToHtml(hd.getHamlDataElements(), html);
+
+            // Temporary:
+            // print Html to console
+            System.out.println(html);
+
+
+            // Pass Html object to Writer
+            //
+            Writer writer = new Writer(html.getInputFileName(), html.getOutputFileName(), html.getHtmlElements());
+            writer.writeToOutputFile();
+
+        }else{
+            // Temporary:
+            // Figure out error handling and error messaging
+            System.out.println("\nHaml file invalid. Error on linenumber Y\n");
+        }
+
     }
 }
