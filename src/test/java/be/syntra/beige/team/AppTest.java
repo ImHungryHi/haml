c2207e2d49cf390ca1a7540d3039000886077f49
jeroennoyez <jeroennoyez@gmail.com>
2021-04-07 00:08:29 +0200

    Added simple test (needs improving)

--- src/test/java/be/syntra/beige/team/AppTest.java
+++ src/test/java/be/syntra/beige/team/AppTest.java
@@ -1,20 +1,42 @@
 package be.syntra.beige.team;
 
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
     /**
-     * Rigorous Test :-)
+     * Simple Haml to Html compile test
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void compileSimpleTags() {
+        // Desired output
+        // Create desired list of HtmlElements
+        ArrayList<String> desiredHtmlElements = new ArrayList<String>(Arrays.asList("<br>","<br>","<br>"));
+
+        // Output to test
+        // Create list of hamlFileLines
+        ArrayList<String> hamlFileLines = new ArrayList<String>(Arrays.asList("%br","%br","%br"));
+        // Create HamlData object
+        HamlData hd = new HamlData("test.haml","test.html");
+        for(String hamlLine : hamlFileLines){
+            hd.hamlDataElements.add(HamlConverter.convertToElement(hamlLine));
+        }
+        // Create Html object
+        Html h = new Html(hd.getInputFileName(),hd.getOutputFileName());
+        HtmlConverter.convertToHtml(hd.getHamlDataElements(), h);
+        // Create list of HtmlElements to test
+        ArrayList<String> realHtmlElements = h.getHtmlElements();
+
+        // Compare 2 lists
+        assertEquals(desiredHtmlElements,realHtmlElements);
     }
 }
