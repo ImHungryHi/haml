eb6b942141a9e0db298f7603178182e93eec6760
jeroennoyez <jeroennoyez@gmail.com>
2021-04-06 21:59:14 +0200

    Added config file containing: - Indentation variable - Empy tags list

---
+++ src/main/java/be/syntra/beige/team/Config.java
@@ -0,0 +1,15 @@
+package be.syntra.beige.team;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Config {
+
+    /**
+     * Define config constants
+     */
+    public static final String INDENTATION = "  ";
+
+    public static final ArrayList<String> EMPTY_TAGS = new ArrayList<String>(Arrays.asList("area","base","br","col","embed","hr","img","input","keygen","link","meta","param","source","track","wbr"));
+
+}
