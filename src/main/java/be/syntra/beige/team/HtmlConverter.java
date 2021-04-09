a2bc0b8f6c5fabd501b50a11393edafa6b4a8c06
ChrisDS <chris.de.smedt.91@gmail.com>
2021-04-09 22:39:36 +0200

    Added recursive function convertToHtml, needs testing, might need cleaning. Also added text and comment functions

--- src/main/java/be/syntra/beige/team/HtmlConverter.java
+++ src/main/java/be/syntra/beige/team/HtmlConverter.java
@@ -2,6 +2,7 @@ package be.syntra.beige.team;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -74,6 +75,92 @@ public class HtmlConverter {
         return endTag;
     }
 
+    private static String createTextContent(HamlDataElement el) {
+        String output = "";
+
+        if (!isEmptyTag(el) && el.hasText()) {
+            String textContent = el.getTextContent();
+
+            if (textContent.contains("\n")) {
+                String indents = createIndentation(el);
+                textContent = textContent.replace("\n", indents + Config.INDENTATION + "\n");   // Fixes indentation for new lines
+                output += "\n" +
+                        indents + Config.INDENTATION + textContent + "\n" +
+                        indents;    // Fixes indentation for closure after text
+            }
+            else {
+                output += textContent;
+            }
+        }
+
+        return output;
+    }
+
+    private static String createCommentContent(HamlDataElement el) {
+        String indents = createIndentation(el);
+        String commentContent = el.getCommentContent();
+
+        if (commentContent != null) {
+            if (commentContent.contains("\n")){
+                return "<!--\n" +
+                        indents + Config.INDENTATION + commentContent.replace("\n", indents + Config.INDENTATION + "\n") + "\n" +
+                        "-->";
+            }
+            else{
+                return "<!-- " + commentContent + " -->";
+            }
+        }
+
+        return null;
+    }
+
+    // Going to rewrite
+    public static void convertToHtml(HamlDataElement el, Html html) {
+        if (!el.isTag()) {
+            String commentContent = createCommentContent(el);
+
+            if (commentContent != null) {
+                html.addElement(commentContent);
+            }
+
+            return;
+        }
+
+        String indents = createIndentation(el);
+        String output = indents + createBeginTag(el);
+        ArrayList<HamlDataElement> children = el.getChildren();
+
+        if (children == null || children.size() < 1) {
+            output += createTextContent(el);
+            output += createEndTag(el);
+            html.addElement(output);
+            return;
+        }
+
+        // We'll get here only if there are child elements that require a new line
+        if (el.hasText()) {
+            String elText = createTextContent(el);
+
+            if (!elText.contains("\n")) {
+                output += "\n" + createIndentation(el) + Config.INDENTATION;
+            }
+
+            output += elText;
+        }
+
+        // Finally, write the begin tag and text to the html list
+        html.addElement(output);
+
+        // Start parsing children
+        for (HamlDataElement child : children) {
+            convertToHtml(child, html);
+        }
+
+        // Close off this element's tag
+        html.addElement(createEndTag(el));
+    }
+
+    /*
     public static void convertToHtml(ArrayList<HamlDataElement> elements, Html html) {
         String htmlElement = "";
         ArrayList<String> htmlElements = new ArrayList<String>();
@@ -143,6 +230,6 @@ public class HtmlConverter {
         }
 
         html.setHtmlElements(htmlElements);
-    }
+    }*/
 
 }
