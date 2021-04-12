563c73a3bd19c9f7be61269b7531130f171e7151
ChrisDS <chris.de.smedt.91@gmail.com>
2021-04-12 13:54:38 +0200

    Root element given as parameter for HTML conversion

--- src/main/java/be/syntra/beige/team/App.java
+++ src/main/java/be/syntra/beige/team/App.java
@@ -45,7 +45,7 @@ public class App {
             // Create Html object
             Html html = new Html(hd.getInputFileName(), hd.getOutputFileName());
             // Pass HamlDataElements to HtmlConverter
-            HtmlConverter.convertToHtml(hd.getHamlDataElements(), html);
+            HtmlConverter.convertToHtml(hd.getHamlDataElements().get(0), html);
 
             // Temporary:
             // print Html to console
