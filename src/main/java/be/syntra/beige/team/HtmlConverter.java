07d59cf9d4dd01204d30bc9528402b6bd0300e2c
ChrisDS <chris.de.smedt.91@gmail.com>
2021-04-21 14:01:01 +0200

    Done with the reordering, implemented WSRM into HtmlConverter

--- src/main/java/be/syntra/beige/team/HtmlConverter.java
+++ src/main/java/be/syntra/beige/team/HtmlConverter.java
@@ -41,60 +41,27 @@ public class HtmlConverter {
         if (el.getCommentType() != null && el.getCommentType().toLowerCase().contains("haml")) {
             return;
         }
-
         if (!el.isTag() && el.isComment()) {
             addCommentContent(el, html);
             return;
         }
-        else if (!el.isTag() && !el.isComment()) {
-            // isText = true
-            String textIndents = createIndentation(el);
-            String textElement = el.getTextContent().replace("\n", "\n" + textIndents);
-            html.addElement(textIndents + textElement);
 
-            // technically this is not allowed to have children - but we could implement setting the children back a level
-            //  maybe something we could do in the future; for now, let's return
-            return;
+        addBeginTag(el, html);
+
+        if (el.hasText() || (!el.isTag() && !el.isComment())) {
+            addTagContent(el, html);
         }
 
-        String indents = createIndentation(el);
-        String output = indents + createBeginTag(el);
         ArrayList<HamlDataElement> children = el.getChildren();
 
-        if (children == null || children.size() < 1) {
-            output += createTextContent(el);
-            output += createEndTag(el);
-            html.addElement(output);
-            return;
-        }
-
-        // We'll get here only if there are child elements that require a new line
-        if (el.hasText()) {
-            String elText = createTextContent(el);
-
-            if (!elText.contains("\n")) {
-                output += "\n" + createIndentation(el) + Config.INDENTATION;
+        if (children != null) {
+            // Shouldn't be iterated if there are none (0 < 0 => false => skip)
+            for (int x = 0; x < children.size(); x++) {
+                convertToHtml(children.get(x), html);
             }
-
-            output += elText;
         }
 
-        // Finally, write the begin tag and text to the html list
-        if ((el.getWhiteSpaceRemovalType() != null && el.getWhiteSpaceRemovalType().equals(">")) ||
-                (el.getParent() != null && el.getParent().getWhiteSpaceRemovalType() != null && el.getParent().getWhiteSpaceRemovalType().equals("<"))) {
-            html.addToPrevious(output);
-        }
-        else {
-            html.addElement(output);
-        }
-
-        // Start parsing children
-        for (HamlDataElement child : children) {
-            convertToHtml(child, html);
-        }
-
-        // Close off this element's tag
-        html.addElement(((el.getChildren().size() > 0 || output.contains("\n")) ? indents : "") + createEndTag(el));
+        addEndTag(el, html);
     }
 
     /**
@@ -140,6 +107,114 @@ public class HtmlConverter {
 
             html.addElement(indents + "-->");
         }
+    }
+
+    private static void addBeginTag(HamlDataElement el, Html html) {
+        String beginTag = createBeginTag(el);
+
+        if (beginTag.equals("")) {
+            return;
+        }
+
+        HamlDataElement parent = el.getParent();
+        HamlDataElement previousSibling = null;
+
+        if (parent != null) {
+            ArrayList<HamlDataElement> siblings = el.getParent().getChildren();
+
+            for (int x = 0; x < siblings.size(); x++) {
+                if (x > 0 && siblings.get(x).equals(el)) {
+                    previousSibling = siblings.get(x - 1);
+                }
+            }
+        }
+
+        // First line checks for parent WSRM
+        //  Second line checks for previous sibling WSRM
+        if (parent != null && parent.hasWhiteSpaceRemoval() && parent.getWhiteSpaceRemovalType().contains("<") ||
+                previousSibling != null && previousSibling.hasWhiteSpaceRemoval() && previousSibling.getWhiteSpaceRemovalType().contains(">")) {
+            html.addToPrevious(beginTag);
+        }
+        else {
+            html.addElement(createIndentation(el) + beginTag);
+        }
+    }
+
+    private static void addTagContent(HamlDataElement el, Html html) {
+        String textContent = createTextContent(el);
+
+        // What do we say to the god of empty lines? Not today!
+        if (textContent.equals("")) {
+            return;
+        }
+
+        HamlDataElement previousSibling = null;
+        boolean isText = false;
+
+        if (!el.isTag() && !el.isComment()) {
+            isText = true;
+
+            if (el.getParent() != null) {
+                // Check for previous siblings
+                ArrayList<HamlDataElement> siblings = el.getParent().getChildren();
+
+                for (int x = 0; x < siblings.size(); x++) {
+                    if (x > 0 && siblings.get(x).equals(el)) {
+                        previousSibling = siblings.get(x - 1);
+                    }
+                }
+            }
+        }
+
+        // Normal situation for single line text. WSRM doesn't do anything different in this case.
+        if (!el.hasChildren() && !textContent.contains("\n")) {
+            html.addToPrevious(textContent);
+        }
+        // First line is no children + multi-line text + inner WSRM
+        //  Second is children + both single-/multi-line text + inner WSRM
+        //  Third is the previous sibling - if found - with outer WSRM
+        else if ((!el.hasChildren() && textContent.contains("\n") && (el.hasWhiteSpaceRemoval() && el.getWhiteSpaceRemovalType().contains("<"))) ||
+                (el.hasChildren() && (el.hasWhiteSpaceRemoval() && el.getWhiteSpaceRemovalType().contains("<"))) ||
+                (previousSibling != null && previousSibling.hasWhiteSpaceRemoval() && previousSibling.getWhiteSpaceRemovalType().contains(">"))) {
+            html.addToPrevious(textContent.replace("\n", "\n" + createIndentation(el)));
+        }
+        else {
+            String indents = createIndentation(el) + (!isText ? Config.INDENTATION : "");
+            html.addElement(indents + textContent.replace("\n", "\n" + indents));
+        }
+    }
+
+    private static void addEndTag(HamlDataElement el, Html html) {
+        String endTag = createEndTag(el);
+
+        // Prevent the code from adding empty lines
+        if (endTag.equals("")) {
+            return;
+        }
+
+        ArrayList<HamlDataElement> children = el.getChildren();
+        HamlDataElement lastChild = null;
+
+        if (el.hasChildren()) {
+            lastChild = children.get(children.size() - 1);
+        }
+
+        boolean hasChildren = el.hasChildren(),
+                childHasWSRM = lastChild != null && lastChild.hasWhiteSpaceRemoval(),
+                childHasOuterWSRM = childHasWSRM && lastChild.getWhiteSpaceRemovalType().contains(">"),
+                hasInnerWSRM = el.hasWhiteSpaceRemoval() && el.getWhiteSpaceRemovalType().contains("<");
+
+        // First line checks for content within the tag that indicates a multi-line text
+        //  The second line checks for WSRM in the last child that forces the closing tag upwards (">")
+        //  The third line checks for WSRM inside the tag itself ("<")
+        if ((!hasChildren && el.hasText() && !el.getTextContent().contains("\n")) ||
+                (hasChildren && childHasWSRM && childHasOuterWSRM) ||
+                hasInnerWSRM) {
+            html.addToPrevious(endTag);
+            return;
+        }
+
+        html.addElement(createIndentation(el) + endTag);
     }
 
     /**
