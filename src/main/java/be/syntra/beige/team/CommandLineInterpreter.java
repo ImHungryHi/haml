e9dfad057e94d3e2a03d590a3e32943f2854b799
Roelens maxim <roelensmaxim@gmail.com>
2021-04-23 00:02:31 +0200

    Update options fixed.

--- src/main/java/be/syntra/beige/team/CommandLineInterpreter.java
+++ src/main/java/be/syntra/beige/team/CommandLineInterpreter.java
@@ -8,6 +8,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 /** interpreter of command lines:
@@ -59,40 +60,57 @@ import java.util.HashMap;
  */
 
 public class CommandLineInterpreter {
-    private String[] args;
-
-    private static String filenameInput;
-    private static String filenameOutput;
-    private ArrayList<String> fileNames;
+    private static ArrayList<String> fileNames;
     private static File dirPath = new File(System.getProperty("user.dir"));
+    private static File testPath = new File(".");
     private boolean update = false;
     private boolean watch = false;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         CommandLineInterpreter interpreter = new CommandLineInterpreter();
         for (String s : args) {
             System.out.println(s);
-
         }
-        System.out.println(dirPath);
+//        System.out.println(dirPath);
+//        System.out.println(dirPath.getAbsolutePath());
+//        System.out.println(testPath.getCanonicalPath());
 
         interpreter.interpretCommand(args);
+        try{
+            for (int i = 0; i< fileNames.size();i++){
+                System.out.println(fileNames.get(i));
+            }
+        }
+         catch (Exception e){
+             System.out.println("fileNames is empty");
+         }
     }
 
-
+    /**
+     *
+     * @param args
+     * Sets the 'filesNames' arraylist with the names of the inputfiles and the names of their respective outputfiles
+     */
     public void interpretCommand(String[] args) {
-        System.out.println(args.length);
+        System.out.println("#arguments=" + args.length + "\n");
+
 
         if (args.length == 0) {
-            System.out.println("No arguments given. Use \"--help\" for commands.");
+            System.out.println("No arguments given. Use \"--help\" for commands.\n");
         } else if (args.length == 1) {
             String command = args[0];
             if (command.equals("--help")) {
-                System.out.println("help commands: ");//showCommands()
+                showCommands();
             } else if (command.equals("--update")) {
-                getFilesToUpdate(); //check html files older than haml files
+                String[] arr = getFilesToUpdate();
+                for(String s : arr){
+                    fileNames.add(s);
+                    fileNames.add(s.split("\\.")[0] + "html");
+                }
+
             } else if (command.equals("--watch")) {
-                watch = true;//initiate watch service
+                watch = true;
+                //initiate watch service
             } else {
                 if (command.matches(".:.")) {
                     fileNames.add(splitCommand(args[0])[0]);
@@ -106,56 +124,76 @@ public class CommandLineInterpreter {
         } else {
             System.out.println("nothing happened");
         }
-
     }
 
 
-    public String[] splitCommand(String s) {
+    public String[] splitCommand(String s){
         return s.split(":");
     }
 
+    public void showCommands(){
+        System.out.println("# Compiles index.haml to index.html.");
+        System.out.println("$ java -jar HamlIt.jar index.haml:index.html \n");
 
-    //gives back a String[] with the haml files that need updating;
-    public void getFilesToUpdate() {
+        System.out.println("# Compiles index.jhaml and contact.jhml to index.html and contact.html.");
+        System.out.println("$ java -jar HamlIt.jar index.haml:index.html contact.haml:contact.html \n");
+
+        System.out.println("# Compiles all haml files in themes/ to html files in public/site/.");
+        System.out.println("$ java -jar HamlIt.jar themes:public/site \n");
+
+        System.out.println("Options:");
+        System.out.println("1) --update  (only compile files that have been modified more recently than the corresponding Html file was generated)");
+        System.out.println("2) --watch (after the first round compilation is done HamlIt stays open and continues compiling files whenever they change.)");
+    }
+
+
+    /**
+    gives back a String[] with the haml files that need updating;
+     **/
+    public String[] getFilesToUpdate() {
         System.out.println("looking for outdated files");
+        System.out.println("/n");
+
         File dir = new File(".");
         if (dir.isDirectory()) {
             String[] files = dir.list(new FilenameFilter() {
-                                          @Override
-                                          public boolean accept(File dir, String name) {
-                                              if (name.endsWith(".haml")) {
-                                                  try {
-                                                      checkBasicAttributes(name);
-                                                  } catch (IOException e) {
-                                                      e.printStackTrace();
-                                                  }
-                                              }
-                                              return false;
-                                          }
-                                      }
+                  @Override
+                  public boolean accept(File dir, String name) {
+                      if (name.endsWith(".haml")) {
+                          try {
+                              return checkBasicAttributes(name);
+                          } catch (IOException e) {
+                              e.printStackTrace();
+                          }
+                      }
+                      return false;
+                  }
+              }
             );
-            System.out.println("Files collected");
-            for (String s : files) {
-                System.out.println(s);
-            }
-        } else System.out.println("error no directory");
+            return files;
+        }
+        System.out.println("error: no directory");
+        return null;
     }
 
+     /**
+     checks which files are outdated based on time
+     of modification in correspondence with their respective outputfiles.
+     **/
     public boolean checkBasicAttributes(String nameHaml) throws IOException {
         Path fileHaml = Paths.get(nameHaml);
         BasicFileAttributes attrHaml = Files.readAttributes(fileHaml, BasicFileAttributes.class);
-        System.out.println("modified time: " + attrHaml.lastModifiedTime());
-        System.out.println(nameHaml);
-        String nameHtml = nameHaml.split(".")[0] + ".html";
-        File test = new File(dirPath + "/" + nameHtml);
-        if (test.isFile()) {
+
+        String nameHtml = nameHaml.split("\\.")[0] + ".html";
+        File fileCheck = new File(dirPath + "/" + nameHtml);
+
+        if (fileCheck.isFile()) {
             System.out.println("output file exists");
             Path fileHtml = Paths.get(nameHtml);
             BasicFileAttributes attrHtml = Files.readAttributes(fileHtml, BasicFileAttributes.class);
-            System.out.println(attrHtml.lastModifiedTime());
+
             return attrHaml.lastModifiedTime().compareTo(attrHtml.lastModifiedTime()) == 1 ? true : false;
             } else {
-            System.out.println(nameHaml + "no output file");
             return true;
             }
 
